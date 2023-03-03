ce4e96082b1658d2e8729720e56237b604b72670
Anonym <anonym516836@gmail.com>
2023-03-04 01:52:21 +0300

    HW_2_2

---
+++ 2_Lesson/HW/HW_2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//напишите программу которая проверяет выходной день
+
+void Prover(int num){
+    if(num <= 5 & num > 0){
+        Console.WriteLine("No");
+    }
+    else if(num >= 6 & num <= 7){
+        Console.WriteLine("YES");
+    }
+    else if(num >= 8 ){
+        Console.WriteLine("ERROOR");
+    }
+}
+Prover(int.Parse(Console.ReadLine()!));
