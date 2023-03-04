0ee7c6260bca4b89b0733a24402f7357bbed6e97
Anonym <anonym516836@gmail.com>
2023-03-04 10:20:34 +0300

    3_1

---
+++ 3_Lesson/3_0/Program.cs
@@ -0,0 +1,18 @@
+﻿void Quarters(int x , int y){
+    if(x ==0 || y == 0){
+        Console.WriteLine("I");
+    }
+    if(x >0 && y > 0){
+        Console.WriteLine("II");
+    }
+    if(x <0 && y > 0){
+        Console.WriteLine("III");
+    }
+    if(x >0 && y < 0){
+        Console.WriteLine("II");
+    }
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Quarters(num1,num2);
---
+++ 3_Lesson/3_1/Program.cs
@@ -0,0 +1,17 @@
+﻿void Koord(int nam){
+    if(nam == 1){
+        Console.WriteLine("x =(0 ... N) , y =(0 ... N)");
+    }
+    else if(nam == 2){
+        Console.WriteLine("x =(0 ... -N) , y =(0 ... N)");
+    }
+    else if(nam == 3){
+        Console.WriteLine("x =(0 ... -N) , y =(0 ... -N)");
+    }
+    else if(nam == 4){
+        Console.WriteLine("x =(0 ... N) , y =(0 ... -N)");
+    }
+    else Console.WriteLine("ERROOOOR");
+}
+ int x = int.Parse(Console.ReadLine()!);
+ Koord(x);
