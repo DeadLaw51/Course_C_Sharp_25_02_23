de7124dadd92a1bbd70e7789d0b2c4e2d4d51ccc
Anonym <anonym516836@gmail.com>
2023-03-05 10:45:01 +0300

    4_3

---
+++ 4_Lesson/4_3/Program.cs
@@ -0,0 +1,10 @@
+﻿void ArrPro(int num){
+    int[] arr = new int[num];
+    for(int i = 0; i <= num; i++){
+        arr[i] = new Random().Next(2);
+        Console.Write(arr[i] + " ");
+    } 
+
+}
+int A = int.Parse(Console.ReadLine()!);
+ArrPro(A);
