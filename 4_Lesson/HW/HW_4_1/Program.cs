b61d542fb23e00069ef1087b827a98cdc57dbe75
Anonym <anonym516836@gmail.com>
2023-03-05 11:16:44 +0300

    HW_4_1

---
+++ 4_Lesson/HW/HW_4_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+void SumNum(int num){
+    int znach = 0 , kol = 0;
+    for(int i = 0; num > 0; i++){
+        znach = num % 10;
+        num /= 10;
+        kol += znach; 
+    }
+    Console.WriteLine(kol);
+}
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine()!);
+SumNum(a);
