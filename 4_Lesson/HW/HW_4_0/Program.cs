b0108cf0faa9ebe5a1fba7ee13901956fa738e37
Anonym <anonym516836@gmail.com>
2023-03-05 11:07:20 +0300

    HW_4_0

---
+++ 4_Lesson/HW/HW_4_0/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void KvadNum(int num, int step){
+    int rezult = 1;
+    for(int i = 0; i < step ; i++ ){
+        rezult *= num;
+    }
+    Console.WriteLine("Результат = " + rezult);
+}
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень в которую хотите ввести число  " + a);
+int b = int.Parse(Console.ReadLine()!);
+KvadNum(a,b);
