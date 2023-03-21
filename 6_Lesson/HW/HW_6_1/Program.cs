34ddeb75a898a97afe2a250243afadfb93b18210
Anonym <anonym516836@gmail.com>
2023-03-21 15:55:18 +0300

    HW_6_0

---
+++ 6_Lesson/HW/HW_6_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Schet(int num){
+    int[] arr = new int[num];
+    int rezult = 0;
+    for(int i = 0;i < num; i++){
+        int a = int.Parse(Console.ReadLine()!);
+        arr[i] = a;
+        if(arr[i] > 0 ){
+                rezult++; 
+        }
+    } 
+    return rezult;
+}
+
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(" aaa= " + Schet(a));
