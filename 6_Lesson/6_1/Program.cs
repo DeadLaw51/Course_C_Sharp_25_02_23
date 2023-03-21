4084b421ed370bc3d42f0895f96d6f67b2590e28
Anonym <anonym516836@gmail.com>
2023-03-21 14:48:47 +0300

    6_1

---
+++ 6_Lesson/6_1/Program.cs
@@ -0,0 +1,12 @@
+﻿void Trin(int a, int b, int c){
+    if(a < b + c && b < a + c && c < a + b ){
+        Console.WriteLine(" Yes");
+    }
+    else Console.WriteLine("Noooo");
+}
+
+int A,B,C;
+A = int.Parse(Console.ReadLine()!);
+B = int.Parse(Console.ReadLine()!);
+C = int.Parse(Console.ReadLine()!);
+Trin(A,B,C);
