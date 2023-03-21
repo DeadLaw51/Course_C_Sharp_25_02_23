2d1f24ca280e00ad0136f8deaafea2c28d44c010
Anonym <anonym516836@gmail.com>
2023-03-21 15:15:51 +0300

    6_2

---
+++ 6_Lesson/6_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 2. Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+
+string Dvo(int num){
+    string rez = "";
+    while(num > 0){
+        rez = num % 2+ rez;
+        num /= 2;
+    }
+    return rez;
+}
+
+//int a = int.Parse(Console.Readline()!);
+Console.WriteLine(Dvo(12));
