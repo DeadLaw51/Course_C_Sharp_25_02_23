73a40251d88b422982c55133022e6dca81ef1bbe
Anonym <anonym516836@gmail.com>
2023-03-22 15:27:17 +0300

    HW_7_1

---
+++ 7_Lesson/7_1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Cozd(int m , int n){
+    double[,] arr = new double[m,n];
+    Random rn = new Random();
+    double rnd= rn.NextDouble();
+    for(int i = 0; i < m;i++){
+        for(int j = 0 ; j < n ; j++){
+            arr[i,j] = (9 * rnd) + 1;
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int stol,str;
+stol = int.Parse(Console.ReadLine()!);
+str = int.Parse(Console.ReadLine()!);
+
+Cozd(stol,str);
