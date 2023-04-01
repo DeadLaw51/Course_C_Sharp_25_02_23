4f7e418c2d8678990b29c230261b9728eb66494a
Anonym <anonym516836@gmail.com>
2023-04-01 14:10:13 +0300

    HW_9_2

---
+++ 9_Lesson/HW/HW_9_2/Program.cs
@@ -0,0 +1,9 @@
+﻿int Val(int n , int m ){
+    if(n < m) return 0;
+    return Val(n - 1,m) + n;
+}
+Console.Write("OT: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("DO: ");
+int mut = int.Parse(Console.ReadLine()!);
+Console.Write(Val(mut,num));
