f949d344178212c5213261bd991a0a6f09d0e1a1
Anonym <anonym516836@gmail.com>
2023-04-01 14:57:05 +0300

    HW_9_1

--- 9_Lesson/HW/HW_9_1/Program.cs
+++ 9_Lesson/HW/HW_9_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Val(int n , int m ){
+    if(n < m) return;
+    Val(n - 1,m);
+    if( n %2 ==0 ){
+    Console.Write(n + " ");}
+}
+Console.Write("OT: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("DO: ");
+int mut = int.Parse(Console.ReadLine()!);
+Val(mut,num);
