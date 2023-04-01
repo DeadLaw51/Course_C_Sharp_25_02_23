48c28d7cd9caadfed441efe51e0a3a81130ce8ad
Anonym <anonym516836@gmail.com>
2023-04-01 13:57:45 +0300

    9_3

---
+++ 9_Lesson/9_3/Program.cs
@@ -0,0 +1,9 @@
+﻿int Val(int n,int s){
+    if(s == 0) return 1;
+    return Val(n , s-1) *n;
+}
+Console.Write("Num: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Stepen: ");
+int rez = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Val(num,rez));
